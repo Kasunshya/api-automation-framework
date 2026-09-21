11de3902a9f25447a4d4b2aa1956a76d69327c98
Kasunshya Pabodi Jayawardana <kasunshyapabodi2002@gmail.com>
2026-09-21 22:15:47 +0530

    Add framework logging

--- src/main/java/com/qe/automation/services/UserService.java
+++ src/main/java/com/qe/automation/services/UserService.java
@@ -4,6 +4,8 @@ import com.qe.automation.endpoints.UserEndpoints;
 import com.qe.automation.models.UserRequest;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static io.restassured.RestAssured.given;
 
@@ -11,58 +13,112 @@ public class UserService {
 
     private final RequestSpecification requestSpec;
 
+    private static final Logger logger =
+            LoggerFactory.getLogger(UserService.class);
+
     public UserService(RequestSpecification requestSpec) {
         this.requestSpec = requestSpec;
     }
 
     public Response getUsers(int page) {
 
-        return given()
+        logger.info("GET users - page: {}", page);
+
+        Response response = given()
                 .spec(requestSpec)
                 .queryParam("page", page)
-
                 .when()
                 .get(UserEndpoints.USERS);
+
+        logger.info(
+                "GET users response status: {}",
+                response.statusCode()
+        );
+
+        return response;
     }
 
     public Response getUser(int userId) {
 
-        return given()
+        logger.info("GET user - ID: {}", userId);
+
+        Response response = given()
                 .spec(requestSpec)
                 .pathParam("userId", userId)
-
                 .when()
                 .get(UserEndpoints.USER_BY_ID);
+
+        logger.info(
+                "GET user response status: {}",
+                response.statusCode()
+        );
+
+        return response;
     }
 
     public Response createUser(UserRequest userRequest) {
 
-        return given()
+        logger.info(
+                "POST create user - name: {}, job: {}",
+                userRequest.getName(),
+                userRequest.getJob()
+        );
+
+        Response response = given()
                 .spec(requestSpec)
                 .body(userRequest)
-
                 .when()
                 .post(UserEndpoints.USERS);
+
+        logger.info(
+                "POST create user response status: {}",
+                response.statusCode()
+        );
+
+        return response;
     }
 
-    public Response updateUser(String userId, UserRequest userRequest) {
+    public Response updateUser(
+            String userId,
+            UserRequest userRequest) {
 
-        return given()
+        logger.info(
+                "PUT update user - ID: {}, name: {}, job: {}",
+                userId,
+                userRequest.getName(),
+                userRequest.getJob()
+        );
+
+        Response response = given()
                 .spec(requestSpec)
                 .pathParam("userId", userId)
                 .body(userRequest)
-
                 .when()
                 .put(UserEndpoints.USER_BY_ID);
+
+        logger.info(
+                "PUT update user response status: {}",
+                response.statusCode()
+        );
+
+        return response;
     }
 
     public Response deleteUser(int userId) {
 
-        return given()
+        logger.info("DELETE user - ID: {}", userId);
+
+        Response response = given()
                 .spec(requestSpec)
                 .pathParam("userId", userId)
-
                 .when()
                 .delete(UserEndpoints.USER_BY_ID);
+
+        logger.info(
+                "DELETE user response status: {}",
+                response.statusCode()
+        );
+
+        return response;
     }
 }
--- src/test/java/com/qe/automation/base/BaseTest.java
+++ src/test/java/com/qe/automation/base/BaseTest.java
@@ -4,11 +4,16 @@ import com.qe.automation.config.ConfigReader;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeClass;
 
 public class BaseTest {
 
     protected RequestSpecification requestSpec;
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(BaseTest.class);
 
     @BeforeClass
     public void setup() {
@@ -19,18 +24,30 @@ public class BaseTest {
         String authToken =
                 ConfigReader.getProperty("authToken");
 
+        logger.info("Starting API test setup");
+        logger.info("Base URL: {}", baseUrl);
+        logger.info(
+                "Authentication configured: {}",
+                authToken != null && !authToken.isBlank()
+        );
+
         RequestSpecBuilder builder =
                 new RequestSpecBuilder()
                         .setBaseUri(baseUrl)
                         .setContentType(ContentType.JSON);
 
         if (authToken != null && !authToken.isBlank()) {
+
             builder.addHeader(
                     "Authorization",
                     "Bearer " + authToken
             );
+
+            logger.info("Authorization header configured");
         }
 
         requestSpec = builder.build();
+
+        logger.info("Request specification created successfully");
     }
 }
